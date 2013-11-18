f98ecb07c8186e1746b170bfe2bae04819ffc1c8
Gregory M Chen <GregoryMChen@gmail.com>
2013-11-18 16:56:46 -0500

    Training of ordinary least squares linear regression model

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn import linear_model
 
 # Load data as a 1D array with structured dtype
 my_data = np.genfromtxt(open("Toronto_weather/noquotes_toronto_1978-2002.csv",'r'), delimiter=',', names=True,dtype="S10,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8,f8")
@@ -7,7 +8,9 @@ my_data = np.genfromtxt(open("Toronto_weather/noquotes_toronto_1978-2002.csv",'r
 my_data_subset = my_data[0:( my_data.size / 5) * 5].reshape(-1,5)
 
 # to do: subset first four columns for train, last column for testing. Then further subset for cross validation..
-train = my_data_subset[:,0:4]
-target = my_data_subset[:,4]
+train = my_data_subset[:,0:4]['Max_Temp_C']
+target = my_data_subset[:,4]['Max_Temp_C']
 
-#target = my_data['Max_Temp_C']
+clf = linear_model.LinearRegression()
+clf.fit(train, target)
+print clf.coef_
